d0c9e15f1f7a2df627a84d092e93fa4ea4a5fb07
Ilya Savinkin <iasavinkin@gmail.com>
2025-02-27 17:34:59 +0300

    updated models and added some samle data in fixtures.json

--- cafe/orders/admin.py
+++ cafe/orders/admin.py
@@ -1,41 +1,46 @@
 from django.contrib import admin
 
-from .models import Order, Item, ItemOrder, Table
+from .models import Item, ItemOrder, Order, Table
 
 
 class ItemInline(admin.TabularInline):
     model = ItemOrder
-    fields = ('item', 'amount')
+    fields = ['item', 'amount', 'get_item_price']
+    readonly_fields = ['get_item_price']
     extra = 1
     verbose_name = 'Строка в заказе'
     verbose_name_plural = 'Строки в заказе'
 
+    @admin.display(description='Цена за единицу')
+    def get_item_price(self, obj):
+        return obj.item.price if obj.item else None
+
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = (
+    list_display = [
         'id', 'table_number', 'total_price',
         'status', 'date_added', 'date_closed',
-    )
-    search_fields = ('table_number',)
-    list_filter = ('table_number', 'status')
-    filter_horizontal = ('items',)
-    inlines = (ItemInline,)
+    ]
+    search_fields = ['table_number']
+    list_filter = ['table_number', 'status']
+    filter_horizontal = ['items']
+    inlines = [ItemInline]
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description', 'price')
-    search_fields = ('title',)
+    list_display = ['id', 'title', 'description', 'price']
+    search_fields = ['title']
 
 
 class ItemOrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'item', 'order', 'amount')
-    search_fields = ('item', 'order')
-    list_filter = ('item',)
+    list_display = ['id', 'item', 'order', 'amount']
+    search_fields = ['item', 'order']
+    list_filter = ['item']
 
 
 class TableAdmin(admin.ModelAdmin):
-    list_display = ('id', 'number')
-    search_fields = ('number',)
+    list_display = ['id', 'number']
+    search_fields = ['number']
 
 
 admin.site.register(Order, OrderAdmin)
--- cafe/orders/models.py
+++ cafe/orders/models.py
@@ -1,28 +1,47 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
+from django.utils import timezone
 
 
 class Table(models.Model):
-    number = models.IntegerField('Номер', unique=True)
+    number = models.SmallIntegerField(
+        'Номер',
+        unique=True,
+        validators=[MinValueValidator(1)],
+    )
 
     class Meta:
         verbose_name = 'Столик'
         verbose_name_plural = 'Столики'
-        ordering = ('number',)
+        ordering = ['number']
 
     def __str__(self):
         return f'Столик №{self.number}'
 
 
 class Item(models.Model):
-    title = models.CharField('Название', max_length=128)
+    title = models.CharField(
+        'Название',
+        max_length=128,
+    )
     description = models.TextField(
-        'Описание', max_length=1024, blank=True, null=True)
-    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+        'Описание',
+        max_length=1024,
+        blank=True,
+        null=True,
+    )
+    price = models.DecimalField(
+        'Цена',
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(1)],
+    )
 
     class Meta:
         verbose_name = 'Блюдо'
         verbose_name_plural = 'Блюда'
-        ordering = ('id',)
+        ordering = ['title']
 
     def __str__(self):
         return self.title
@@ -30,22 +49,48 @@ class Item(models.Model):
 
 class ItemOrder(models.Model):
     item = models.ForeignKey(
-        Item, on_delete=models.CASCADE, related_name='orders',
-        verbose_name='Блюдо'
+        Item,
+        on_delete=models.CASCADE,
+        related_name='orders',
+        verbose_name='Блюдо',
     )
     order = models.ForeignKey(
-        'Order', on_delete=models.CASCADE, related_name='items_in_oreder',
-        verbose_name='Заказ'
+        'Order',
+        on_delete=models.CASCADE,
+        related_name='items_in_oreder',
+        verbose_name='Заказ',
     )
-    amount = models.PositiveIntegerField('Количество')
+    amount = models.PositiveIntegerField(
+        'Количество',
+        validators=[MinValueValidator(1)],
+        default=1,
+    )
 
     class Meta:
         verbose_name = 'Позиция в заказе'
         verbose_name_plural = 'Позиции в заказах'
-        ordering = ('order', 'item')
+        ordering = ['order', 'item']
 
     def __str__(self):
         return f'{self.amount} × {self.item.title} (Заказ {self.order.id})'
+
+    def save(self, *args, **kwargs):
+        """Update total_price on change."""
+        if self.pk:
+            old_amount = ItemOrder.objects.get(pk=self.pk).amount
+            difference = (self.amount - old_amount) * self.item.price
+            self.order.total_price += difference
+        else:
+            self.order.total_price += self.amount * self.item.price
+
+        super().save(*args, **kwargs)
+        self.order.save(update_fields=['total_price'])
+
+    def delete(self, *args, **kwargs):
+        """Update total_price on delete."""
+        self.order.total_price -= self.amount * self.item.price
+        super().delete(*args, **kwargs)
+        self.order.save(update_fields=['total_price'])
 
 
 class Order(models.Model):
@@ -55,21 +100,46 @@ class Order(models.Model):
         ('PAID', 'оплачено'),
     ]
     table_number = models.ForeignKey(
-        Table, on_delete=models.SET_NULL, null=True, blank=True)
+        Table,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
     items = models.ManyToManyField(
-        Item, through=ItemOrder, verbose_name='Блюда в заказе')
+        Item,
+        through=ItemOrder,
+        verbose_name='Блюда в заказе',
+    )
     total_price = models.DecimalField(
         'Итоговая стоимость заказа',
-        max_digits=10, decimal_places=2, default=0)
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        validators=[MinValueValidator(0)],
+        editable=False,
+    )
     status = models.CharField(
-        'Статус', max_length=8, choices=STATUS_CHOICES, default='PENDING')
-    date_added = models.DateTimeField('Время создания', auto_now_add=True)
-    date_closed = models.DateTimeField('Время закрытия', null=True, blank=True)
+        'Статус',
+        max_length=8,
+        choices=STATUS_CHOICES,
+        default='PENDING',
+    )
+    date_added = models.DateTimeField(
+        'Время создания',
+        auto_now_add=True,
+        editable=False,
+    )
+    date_closed = models.DateTimeField(
+        'Время закрытия',
+        null=True,
+        blank=True,
+        editable=False,
+    )
 
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
-        ordering = ('id', 'status')
+        ordering = ['status', 'date_added']
 
     def __str__(self):
         return (
@@ -77,9 +147,28 @@ class Order(models.Model):
             f"- {self.get_status_display()}"
         )
 
+    def clean_table_number(self):
+        """Forbid empty table on creation, but allow null if table deleted."""
+        if not self.table_number and not self.pk:
+            raise ValidationError('Выберите стол перед созданием заказа!')
+
+    def clean(self):
+        self.clean_table_number()
+
+    def save(self, *args, **kwargs):
+        """Update date_closed when order is paid."""
+        self.full_clean()
+        if self.status == 'PAID' and self.date_closed is None:
+            self.date_closed = timezone.now()
+        elif self.status != 'PAID':
+            self.date_closed = None
+
+        super().save(*args, **kwargs)
+
     def calculate_total_price(self):
         total = sum(
-            item.item.price * item.amount for item in self.items_in_order.all()
+            item.item.price * item.amount
+            for item in self.items_in_order.all()
         )
         self.total_price = total
         self.save()
